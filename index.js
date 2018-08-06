0356869286af22ba851e4f7738fbe6837f565d1c
C-Lion Stepansky <esstepansky@gmail.com>
2018-08-06 22:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Code your solution here!
+function printString(myString) {
+  let substring;
+
+  //console.log(myString);
+  console.log(myString[0]);
+
+  if(myString.length > 1) {
+    substring = myString.substring(1);
+    //console.log(substring);
+    printString(substring);
+  } else {
+    console.log(substring);
+    return true;
+  }
+}
+ 
+printString("Babycat");
 
